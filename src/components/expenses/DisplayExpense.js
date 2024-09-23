ee1ebc2058546f2b8ff2740d6fe2e4e50e0acd9a
sameer798 <sameershams.adi@gmailcom>
2024-09-23 17:27:38 +0530

    POST and GET requesting from firebase for expenses

--- src/components/expenses/DisplayExpense.js
+++ src/components/expenses/DisplayExpense.js
@@ -4,7 +4,7 @@ function DisplayExpense({ expense }) {
   return (
     <>
       <div className="flex flex-col justify-center items-center mt-5">
-        {/* Centering the heading above the table */}
+      
         <h2 className="text-xl font-bold mb-3">Expenses</h2>
         <table className="min-w-full bg-white border border-gray-300">
           <thead>
@@ -17,7 +17,7 @@ function DisplayExpense({ expense }) {
           <tbody>
             {expense.map((item, index) => (
               <tr
-                key={index}
+                key={item.id}
                 className={`${index % 2 === 0 ? "bg-white" : "bg-green-200"} text-gray-800`}
               >
                 <td className="py-2 px-4 border-b border-gray-300">{item.category}</td>
--- src/components/expenses/NewExpense.js
+++ src/components/expenses/NewExpense.js
@@ -1,62 +1,126 @@
-import React, {useRef, useState} from "react";
+import React, { useEffect, useRef, useState } from "react";
 import DisplayExpense from "./DisplayExpense";
 
 function NewExpense() {
-    const [newExpense, setNewExpense] = useState([]);
-    const enteredSpentRef = useRef();
-    const enteredDescriptionRef = useRef();
-    const enteredCategoryRef = useRef();
+  const [newExpense, setNewExpense] = useState([]);
+  const enteredSpentRef = useRef();
+  const enteredDescriptionRef = useRef();
+  const enteredCategoryRef = useRef();
 
-     
-   
+  const getExpenseDataFromFirebase = async() =>{
+    try {
+        const response = await fetch("https://react-expense-5b6f8-default-rtdb.firebaseio.com/expenses.json");
+    const data = await response.json();
 
-    const expenseSubmitHandler =(e)=>{
-        e.preventDefault();
-        const spent = enteredSpentRef.current.value;
-        const description = enteredDescriptionRef.current.value;
-        const category = enteredCategoryRef.current.value;
-        const newExpenseData = {
-            spent,
-            description,
-            category
-        }
-       
-        setNewExpense((prevExpense)=>{
-            if(!Array.isArray(prevExpense)){
-                return [prevExpense]
-            }else{
-                return [...prevExpense, newExpenseData]
-            }
+    const loadedExpenses = [];
+
+    for(const key in data){
+        loadedExpenses.push({
+            id: key,
+            ...data[key]
         })
-        
-        enteredSpentRef.current.value = '';
-        enteredDescriptionRef.current.value = '';
     }
+    setNewExpense(loadedExpenses)
+    } catch (error) {
+        console.log("error", error)
+    }
+  }
 
+    useEffect(()=>{
+        getExpenseDataFromFirebase()
+    },[newExpense])
 
-   console.log(newExpense)
+  const expenseSubmitHandler = async (e) => {
+    e.preventDefault();
+    const spent = enteredSpentRef.current.value;
+    const description = enteredDescriptionRef.current.value;
+    const category = enteredCategoryRef.current.value;
+    const newExpenseData = {
+      spent,
+      description,
+      category,
+    };
+    try {
+      const response = await fetch(
+        "https://react-expense-5b6f8-default-rtdb.firebaseio.com/expenses.json",
+        {
+          method: "POST",
+          body: JSON.stringify(newExpenseData),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+    
+      const data = await response.json();
+    //   if (response.ok) {
+    //     setNewExpense((prevExpense) => {
+    //       if (!Array.isArray(prevExpense)) {
+    //         return [prevExpense];
+    //       } else {
+    //         return [...prevExpense, newExpenseData];
+    //       }
+    //     });
+
+    //   }else{
+    //     return
+    //   }
+    //   console.log(data);
+
+    // if (response.ok) {
+    //     // Update the local state by adding the new expense
+    //     setNewExpense((prevExpense) => [
+    //       ...prevExpense,
+    //       { id: data.name, ...newExpenseData }, 
+    //     ]);
+    //   }
+
+    } catch (error) {}
+
+    enteredSpentRef.current.value = "";
+    enteredDescriptionRef.current.value = "";
+  };
+
   return (
     <>
       <div className="bg-green-300 h-14 text-center pt-3 font-bold">
         Day to Day Expenses
       </div>
 
-
       <div className="mt-7 flex justify-center">
         <form onSubmit={expenseSubmitHandler}>
-          <input type="number" placeholder="spent"  className="ml-2  border-b-2 " ref={enteredSpentRef}/>
-          <input type="text" placeholder="description" className="ml-2 border-b-2" ref={enteredDescriptionRef}/>
-          <label htmlFor="category" className="ml-2">category:</label>
+          <input
+            type="number"
+            placeholder="spent"
+            className="ml-2  border-b-2 "
+            ref={enteredSpentRef}
+          />
+          <input
+            type="text"
+            placeholder="description"
+            className="ml-2 border-b-2"
+            ref={enteredDescriptionRef}
+          />
+          <label htmlFor="category" className="ml-2">
+            category:
+          </label>
           <select name="category" id="category" ref={enteredCategoryRef}>
             <option value="volvo">Food</option>
-            <option value="saab">Petrol</option>
-            <option value="opel">Shopping</option>
-            <option value="audi">Grocery</option>
+            <option value="Petrol">Petrol</option>
+            <option value="Shopping">Shopping</option>
+            <option value="Grocery">Grocery</option>
           </select>
-          <button className=" ml-2 font-serif text-sm  bg-gradient-to-r from-purple-500 to-pink-500 text-white p-1 rounded hover:bg-gradient-to-l">Add expense</button>
+          <button className=" ml-2 font-serif text-sm  bg-gradient-to-r from-purple-500 to-pink-500 text-white p-1 rounded hover:bg-gradient-to-l">
+            Add expense
+          </button>
         </form>
       </div>
-      {newExpense.length === 0 ? <p className="font-serif text-center mt-8 ">No expenses</p> : <DisplayExpense expense={newExpense}/> }
+      {newExpense.length === 0 ? (
+        <p className="font-serif text-center mt-8 ">No expenses</p>
+      ) : (
+        <DisplayExpense expense={newExpense} />
+      )}
     </>
   );
 }
